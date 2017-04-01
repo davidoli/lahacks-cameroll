a92cd1c4c6a260ffbdd539de7dd52b3dd5f3dbb5
Your Name <dil048@ucsd.edu>
2017-04-01 13:17:32 -0700

    All set for layout, starting java

--- CamerRoll/app/src/main/java/com/example/dianlin/camerroll/MainActivity.java
+++ CamerRoll/app/src/main/java/com/example/dianlin/camerroll/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.dianlin.camerroll;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.ImageButton;
 public class MainActivity extends AppCompatActivity {
 
     @Override
